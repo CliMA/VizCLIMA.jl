4b86f03cbdac7f41f12b1f03147e8f9afc0d4a5e
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-03-13 02:57:40 +0000

    build based on 13dcba2

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"index.html#VizCLIMA.jl-1","page":"Home","title":"VizCLIMA.jl","text":"","category":"section"},{"location":"index.html#","page":"Home","title":"Home","text":"A toolkit for visualizing CLIMA's results","category":"page"}]
+}
