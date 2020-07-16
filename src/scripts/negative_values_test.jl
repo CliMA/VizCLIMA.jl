20320908384ba6374d2f8052f4ea8bd3f9d14938
Anna Jaruga <ajaruga@caltech.edu>
2020-07-15 17:41:40 -0700

    ...

--- src/scripts/negative_values_test.jl
+++ src/scripts/negative_values_test.jl
@@ -39,7 +39,7 @@ data_a = Dict(var => ncread(file_a, var) for var in var_a)
 #(x, y, z, t)
 #@info(size(data_a["w"]))
 
-time_idx = 5
+time_idx = 11
 y_idx    = 5
 
 ylab = "z [km]"
@@ -62,11 +62,11 @@ p2 = contourf(x, z, transpose(data_a["e_pot"][:, y_idx, :, time_idx]),          
 p3 = contourf(x, z, transpose(data_a["e_kin"][:, y_idx, :, time_idx]), ylabel=ylab, levels=nlev, title="e_kin", color=:haline)
 p4 = contourf(x, z, transpose(data_a["e_int"][:, y_idx, :, time_idx]),              levels=nlev, title="e_int", color=:haline)
 p5 = plot(legend=false,grid=false,foreground_color_subplot=:white)
-if min.(data_a["rain_w"][:, y_idx, :, time_idx]) != max.(data_a["rain_w"][:, y_idx, :, time_idx])
-    p5 = contourf(x, z, transpose(data_s["rain_w"][:, y_idx, :, time_idx]), ylabel=ylab, xlabel=xlab, levels=nlev, title="rain_w [m/s]", color=:haline)
+if minimum(data_a["rain_w"][:, y_idx, :, time_idx]) != maximum(data_a["rain_w"][:, y_idx, :, time_idx])
+    p5 = contourf(x, z, transpose(data_a["rain_w"][:, y_idx, :, time_idx]), ylabel=ylab, xlabel=xlab, levels=nlev, title="rain_w [m/s]", color=:haline)
 end
 p6 = plot(legend=false,grid=false,foreground_color_subplot=:white)
-if min.(data_a["snow_w"][:, y_idx, :, time_idx]) != max.(data_a["snow_w"][:, y_idx, :, time_idx])
+if minimum(data_a["snow_w"][:, y_idx, :, time_idx]) != maximum(data_a["snow_w"][:, y_idx, :, time_idx])
     p6 = contourf(x, z, transpose(data_a["snow_w"][:, y_idx, :, time_idx]), xlabel=xlab, levels=nlev, title="snow_w [m/s]", color=:haline)
 end
 fig = plot(p1, p2, p3, p4, p5, p6, layout=(3, 2), size=(1500, 1000))
@@ -76,19 +76,19 @@ savefig(fig, string(analysis_folder, "quicklook_energy_and_velocity.pdf"))
 p1 = contourf(x, z, transpose(data_a["q_tot"][:, y_idx, :, time_idx])*1e3, ylabel=ylab, levels=nlev, title="q_tot [g/kg]", color=:haline)
 p2 = contourf(x, z, transpose(data_a["q_vap"][:, y_idx, :, time_idx])*1e3,              levels=nlev, title="q_vap [g/kg]", color=:haline)
 p3 = plot(legend=false,grid=false,foreground_color_subplot=:white)
-if min.(data_a["q_liq"][:, y_idx, :, time_idx]) != max.(data_a["q_liq"][:, y_idx, :, time_idx])
+if minimum(data_a["q_liq"][:, y_idx, :, time_idx]) != maximum(data_a["q_liq"][:, y_idx, :, time_idx])
     p3 = contourf(x, z, transpose(data_a["q_liq"][:, y_idx, :, time_idx])*1e3, ylabel=ylab, levels=nlev, title="q_liq [g/kg]", color=:haline)
 end
 p4 = plot(legend=false,grid=false,foreground_color_subplot=:white)
-if min.(data_a["q_ice"][:, y_idx, :, time_idx]) != max.(data_a["q_ice"][:, y_idx, :, time_idx])
+if minimum(data_a["q_ice"][:, y_idx, :, time_idx]) != maximum(data_a["q_ice"][:, y_idx, :, time_idx])
     p4 = contourf(x, z, transpose(data_a["q_ice"][:, y_idx, :, time_idx])*1e3, levels=nlev, title="q_ice [g/kg]", color=:haline)
 end
 p5 = plot(legend=false,grid=false,foreground_color_subplot=:white)
-if min.(data_a["q_rai"][:, y_idx, :, time_idx]) != max.(data_a["q_rai"][:, y_idx, :, time_idx])
+if minimum(data_a["q_rai"][:, y_idx, :, time_idx]) != maximum(data_a["q_rai"][:, y_idx, :, time_idx])
     p5 = contourf(x, z, transpose(data_a["q_rai"][:, y_idx, :, time_idx])*1e3, ylabel=ylab, xlabel=xlab, levels=nlev, title="q_rai [g/kg]", color=:haline)
 end
 p6 = plot(legend=false,grid=false,foreground_color_subplot=:white)
-if min.(data_a["q_sno"][:, y_idx, :, time_idx]) != max.(data_a["q_sno"][:, y_idx, :, time_idx])
+if minimum(data_a["q_sno"][:, y_idx, :, time_idx]) != maximum(data_a["q_sno"][:, y_idx, :, time_idx])
     p6 = contourf(x, z, transpose(data_a["q_sno"][:, y_idx, :, time_idx])*1e3, xlabel=xlab, levels=nlev, title="q_sno [g/kg]", color=:haline)
 end
 fig = plot(p1, p2, p3, p4, p5, p6, layout=(3, 2), size=(1500, 1000))
@@ -96,27 +96,27 @@ savefig(fig, string(analysis_folder, "quicklook_microph.pdf"))
 
 # flags and supersaturations
 p1 = plot(legend=false,grid=false,foreground_color_subplot=:white)
-if min.(data_a["S_liq"][:, y_idx, :, time_idx]) != max.(data_a["S_liq"][:, y_idx, :, time_idx])
+if minimum(data_a["S_liq"][:, y_idx, :, time_idx]) != maximum(data_a["S_liq"][:, y_idx, :, time_idx])
     p1 = contourf(x, z, transpose(data_a["S_liq"][:, y_idx, :, time_idx])*1e3, ylabel=ylab, levels=nlev, title="S_liq [-]", color=:haline)
 end
 p2 = plot(legend=false,grid=false,foreground_color_subplot=:white)
-if min.(data_a["S_ice"][:, y_idx, :, time_idx]) != max.(data_a["S_ice"][:, y_idx, :, time_idx])
+if minimum(data_a["S_ice"][:, y_idx, :, time_idx]) != maximum(data_a["S_ice"][:, y_idx, :, time_idx])
     p2 = contourf(x, z, transpose(data_a["S_ice"][:, y_idx, :, time_idx])*1e3, levels=nlev, title="S_ice [-]", color=:haline)
 end
 p3 = plot(legend=false,grid=false,foreground_color_subplot=:white)
-if min.(data_a["flag_cloud_liq"][:, y_idx, :, time_idx]) != max.(data_a["flag_cloud_liq"][:, y_idx, :, time_idx])
+if minimum(data_a["flag_cloud_liq"][:, y_idx, :, time_idx]) != maximum(data_a["flag_cloud_liq"][:, y_idx, :, time_idx])
     p3 = contourf(x, z, transpose(data_a["flag_cloud_liq"][:, y_idx, :, time_idx])*1e3, ylabel=ylab, levels=nlev, title="flag cloud liq", color=:haline)
 end
 p4 = plot(legend=false,grid=false,foreground_color_subplot=:white)
-if min.(data_a["flag_cloud_ice"][:, y_idx, :, time_idx]) != max.(data_a["flag_cloud_ice"][:, y_idx, :, time_idx])
+if minimum(data_a["flag_cloud_ice"][:, y_idx, :, time_idx]) != maximum(data_a["flag_cloud_ice"][:, y_idx, :, time_idx])
     p4 = contourf(x, z, transpose(data_a["flag_cloud_ice"][:, y_idx, :, time_idx])*1e3, levels=nlev, title="flag cloud ice", color=:haline)
 end
 p5 = plot(legend=false,grid=false,foreground_color_subplot=:white)
-if min.(data_a["flag_rain"][:, y_idx, :, time_idx]) != max.(data_a["flag_rain"][:, y_idx, :, time_idx])
+if minimum(data_a["flag_rain"][:, y_idx, :, time_idx]) != maximum(data_a["flag_rain"][:, y_idx, :, time_idx])
     p5 = contourf(x, z, transpose(data_a["flag_rain"][:, y_idx, :, time_idx])*1e3, ylabel=ylab, xlabel=xlab, levels=nlev, title="flag rain", color=:haline)
 end
 p6 = plot(legend=false,grid=false,foreground_color_subplot=:white)
-if min.(data_a["flag_snow"][:, y_idx, :, time_idx]) != max.(data_a["flag_snow"][:, y_idx, :, time_idx])
+if minimum(data_a["flag_snow"][:, y_idx, :, time_idx]) != maximum(data_a["flag_snow"][:, y_idx, :, time_idx])
     p6 = contourf(x, z, transpose(data_a["flag_snow"][:, y_idx, :, time_idx])*1e3, xlabel=xlab, levels=nlev, title="flag snow", color=:haline)
 end
 fig = plot(p1, p2, p3, p4, p5, p6, layout=(3, 2), size=(1500, 1000))
