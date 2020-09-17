3b93254fe4d6f51694e64f6d4b773b4a13eb7c54
lenka <lenka@caltech.edu>
2020-09-17 13:57:44 -0700

    clean basic

--- src/scripts/general-gcm-notebook-setup.jl
+++ src/scripts/general-gcm-notebook-setup.jl
@@ -2,11 +2,9 @@
 using Plots
 using NCDatasets
 using Statistics: mean
-using DelimitedFiles
-using PrettyTables
-using PaddedViews
-using Dates
-using Printf
+
+# Setup run-time enviromnent
+ENV["GKSwstype"] = "100"
 
 # Specify needed directories and filenames: these will be replaced automatically by the bash run script or can be modified manually
 CLIMA_NETCDF = "../netcdf/";
@@ -16,7 +14,7 @@ fnames = filter(x -> occursin(".nc", x), readdir( CLIMA_NETCDF ) );
 filename = "$CLIMA_NETCDF"fnames[end] # selects the last file on list
 
 # extract data
-ds = NCDataset(file_name, "r");
+ds = NCDataset(filename, "r");
 lon = ds["long"][:];
 lat = ds["lat"][:];
 lev = ds["level"][:] / 1e3; # height in kilometers
@@ -28,17 +26,21 @@ close(ds)
 
 # zonal mean:T and u at last diagnostic time
 time_index = length(time)
-u_zm = mean( u[:,:,:,:], dims=1); # lon, lat,lev, time
-T_zm = mean( T[:,:,:,:], dims=1); # lon, lat,lev, time
+u_zm = mean( u[:,:,:,:], dims=1)[1,:,:,:]; # lon, lat,lev, time
+T_zm = mean( T[:,:,:,:], dims=1)[1,:,:,:]; # lon, lat,lev, time
 
 plot1 = contourf( lat, lev, (u_zm[:,:,time_index])', title="u", xlabel="lat (deg N)", ylabel="z (km)");
 plot_array = [plot1]
 plot2 = contourf( lat, lev, (T_zm[:,:,time_index])', title="T", xlabel="lat (deg N)", ylabel="z (km)");
 push!(plot_array,plot2);
 fig=plot(plot_array... , layout=(1, 2), size=(800, 400) )
+savefig(fig, "zontal_mean.pdf");
+display(fig)
 
 # vertical slice of v at lev_index
 lev_index = 10
 z_in_km = lev[lev_index]
-v_plot = contourf( lon, lat, ( (hs[:,:,1,1]-hs_c[:,:,1,1])*1.0 )', title="v @ $z_in_km", xlabel="lon (deg)", ylabel="lat (deg N)");
+v_plot = contourf( lon, lat, (v[:,:,lev_index,time_index])', title="v @ $z_in_km", xlabel="lon (deg)", ylabel="lat (deg N)");
 fig=plot(v_plot, layout=(1, 1), size=(800, 400) )
+savefig(fig, "vertical_slice.pdf");
+display(fig)
