0ac86330759e0968fc0d661fdf5ad76372b7b5eb
Charles Kawczynski <kawczynski.charles@gmail.com>
2021-01-13 17:29:02 -0800

    Test docs

--- docs/make.jl
+++ docs/make.jl
@@ -1,30 +1,30 @@
 using VizCLIMA, Documenter
 
 pages = Any[
-  "Home" => "index.md",
-  "VizCLIMA Scripts" => "vizclima_script_examples.md",
-  "End-to-end Modeling (SLURM)" => "slurm_users_end_to_end.md",
-  "Nimbus" => "datavis.md",
-  "Gallery" => "gallery.md",
-  "Third party software" => "third_party.md"
- ]
+    "Home" => "index.md",
+    "VizCLIMA Scripts" => "vizclima_script_examples.md",
+    "End-to-end Modeling (SLURM)" => "slurm_users_end_to_end.md",
+    "Nimbus" => "datavis.md",
+    "Gallery" => "gallery.md",
+    "Third party software" => "third_party.md"
+]
 
 makedocs(
-  sitename = "VizCLIMA.jl",
-  doctest = false,
-  strict = true,
-  format = Documenter.HTML(
-        prettyurls = get(ENV, "CI", nothing) == "true",
-        mathengine = MathJax(Dict(
-            :TeX => Dict(
-                :equationNumbers => Dict(:autoNumber => "AMS"),
-                :Macros => Dict()
-            )
-        ))
-  ),
-  clean = false,
-  modules = [VizCLIMA],
-  pages = pages,
+    sitename = "VizCLIMA.jl",
+    doctest = false,
+    strict = true,
+    format = Documenter.HTML(
+          prettyurls = get(ENV, "CI", nothing) == "true",
+          mathengine = MathJax(Dict(
+              :TeX => Dict(
+                  :equationNumbers => Dict(:autoNumber => "AMS"),
+                  :Macros => Dict()
+              )
+          ))
+    ),
+    clean = false,
+    modules = [VizCLIMA],
+    pages = pages,
 )
 
 deploydocs(
